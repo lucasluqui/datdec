0847e7b83819688eaa4a7113ca48696dc1084d33
Ray J. Greenwell <ray@bogocorp.com>
2025-10-26 15:10:47 -0700

    Just use the stuff built-in to clyde?
    The classes in clyde could replace this entire "Datdec" tool but here's
    code that should keep things working the way you're used to...

--- src/com/lucasallegri/datdec/compile/Compile.java
+++ src/com/lucasallegri/datdec/compile/Compile.java
@@ -8,61 +8,37 @@ import java.util.List;
 
 import com.lucasallegri.datdec.gui.DatdecContext;
 import com.lucasallegri.datdec.gui.DatdecGUI;
-import com.lucasallegri.datdec.ooo.OOOImporter;
 import com.lucasallegri.datdec.gui.util.ConfigChoice;
 import com.lucasallegri.datdec.util.FileUtil;
 import com.lucasallegri.datdec.util.PathUtil;
 
+import com.threerings.export.tools.XMLToBinaryConverter;
+
 public class Compile {
-	
+  private static final boolean COMPRESS = true;
+
 	public static void compile() {
-		
-		File source = new File(PathUtil.getPathToConfig(DatdecContext.selectedConfig));
-		FileInputStream in = null;
+    convert(DatdecContext.selectedConfig);
+	}
+
+	public static int compileAll() {
+    List<String> list = FileUtil.fileNamesInDirectory("rsrc/config/");
+    for (String file : list) {
+			if (file.endsWith(".xml")) convert(file);
+		}
+		return list.size(); // why not return the number that were converted?
+	}
+
+  private static void convert(String file) {
+		File source = new File(PathUtil.getPathToConfig(file));
 		String path = source.getAbsolutePath();
-		
 		try {
-			
-			in = new FileInputStream(source);
 			String dest = path.replaceFirst("\\.xml$", ".dat");
-			if(DatdecContext.doBackups) FileUtil.backupFile(dest);
-			FileOutputStream out = new FileOutputStream(dest);
-			OOOImporter._import(in, out);
+			if (DatdecContext.doBackups) FileUtil.backupFile(dest);
+      XMLToBinaryConverter.convert(path, dest, COMPRESS);
 			source.delete();
-			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-	}
-	
-	public static int compileAll() {
-		
-		List<String> _list = FileUtil.fileNamesInDirectory("rsrc/config/");
-		
-		for(int i = 0; i < _list.size(); i++) {
-			
-			if(_list.get(i).endsWith(".dat")) continue;
-			
-			File source = new File(PathUtil.getPathToConfig(_list.get(i)));
-			FileInputStream in = null;
-			String path = source.getAbsolutePath();
-			
-			try {
-				
-				in = new FileInputStream(source);
-				String dest = path.replaceFirst("\\.xml$", ".dat");
-				if(DatdecContext.doBackups) FileUtil.backupFile(dest);
-				FileOutputStream out = new FileOutputStream(dest);
-				OOOImporter._import(in, out);
-				source.delete();
-				
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		
-		return _list.size();
-		
-	}
-
+  }
 }
