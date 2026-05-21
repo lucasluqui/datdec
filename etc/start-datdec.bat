04232a08b5b8e616a5e482dead52e91248e0832d
Lucas <lucasallegri@protonmail.com>
2026-05-20 22:34:07 -0300

    Fixes to windows batch script to adapt for all runtime locations.

--- etc/start-datdec.bat
+++ etc/start-datdec.bat
@@ -1,3 +1,16 @@
-set here=%cd%
-"%here%\.\java_vm\bin\java.exe" -classpath "%here%\.\datdec.jar;%here%\.\code\projectx-pcode.jar" com.lucasluqui.datdec.cli.DatdecCLI
+@echo off
+setlocal
+
+set "here=%cd%"
+set "javaExe="
+
+:: Check whether java_vm exists, if not fall back to runtime.
+if exist "%here%\.\java_vm\bin\java.exe" (
+    set "javaExe=%here%\.\java_vm\bin\java.exe"
+) else if exist "%here%\..\runtime\bin\java.exe" (
+    set "javaExe=%here%\..\runtime\bin\java.exe"
+)
+
+"%javaExe%" -classpath "%here%\.\datdec.jar;%here%\.\code\projectx-pcode.jar" com.lucasluqui.datdec.cli.DatdecCLI
+
 pause
